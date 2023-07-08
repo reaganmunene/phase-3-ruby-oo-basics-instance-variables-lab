858d67a57f7389bc7af5deddb0037e9b8dc02cd5
reaganmunene <reagannmunene@gmail.com>
2023-07-08 18:20:44 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
 
+def name=(dog_name) #note that this is a 'setter' method bc of the equal sign
+  @this_dogs_name= dog_name
+end
+
+def name #This is a 'getter' method. it's responsible for showing the attribute of a variable only
+  @this_dogs_name
+end
+
+
 end
